01bd1e1d08d4d5e778217db773925415b374f740
nyanyan <46193012+zentox@users.noreply.github.com>
2024-10-13 22:41:52 +0200

    FopBot Dependency (#2)
    * Initial template
    
    * Match first and last name with all other repositories
    
    * Add jitpack
    
    ---------
    
    Co-authored-by: nyanyan <zentox@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ dependencyResolutionManagement {
     repositories {
 //        mavenLocal()
         maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+        maven("https://jitpack.io")
         mavenCentral()
     }
 }
